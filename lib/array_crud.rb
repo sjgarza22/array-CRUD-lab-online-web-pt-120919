3bcf2f766ab95d9a6127ff58e4d1f5e8567430ba
Sara Garza <sjgarza22@gmail.com>
2019-12-17 00:01:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,17 +15,14 @@ end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
-  array
 end
 
 def remove_element_from_end_of_array(array)
   array.pop
-  array
 end
 
 def remove_element_from_start_of_array(array)
   array.shift
-  array
 end
 
 def retrieve_element_from_index(array, index_number)
